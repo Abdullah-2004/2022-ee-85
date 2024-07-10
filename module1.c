cd1c80f44fed1708d344f94b7d95757add042837
Abdullah Shafiq <abdullah.qureshi5341@gmail.com>
2024-07-10 14:36:51 +0500

    code

---
+++ module1.c
@@ -0,0 +1,35 @@
+int main(){
+     int A;
+     int Q;
+     int M;
+     int N;
+
+     scanf("enter your dividend: %d", &A);
+     scanf("enter your divisor:%d",&M);
+
+     A=0;
+     N=log2(Q);
+     while(N!=0){
+        var=A;
+        A=A-M;
+     }
+
+int binary(int n){
+    int i=0;
+    int x[12];
+    while(num==2){
+        x[i]=num/2;
+        n=n-x[i]/2;
+        printf("%d",x[i])
+        i++;
+    }
+    return x;
+int array(int*a,int*b){
+    int b1=sizeof(a);
+    int b2=sizeof(b);
+
+}
+
+int pointer()
+}
+
